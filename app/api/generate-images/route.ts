466ab7dc6170787c6aeeb3eae1e8a4dfef7077f0
Aman <techmasters@infinityassurance.com>
2025-04-11 14:26:35 +0530

    warranty image

--- app/api/generate-images/route.ts
+++ app/api/generate-images/route.ts
@@ -8,6 +8,7 @@ import JSZip from "jszip";
 import { writeFile } from "fs/promises";
 import { tmpdir } from "os";
 import { join } from "path";
+import { readFileSync } from "fs";
 
 export async function POST(req: NextRequest) {
   try {
@@ -29,9 +30,20 @@ export async function POST(req: NextRequest) {
 
     const workbook = XLSX.read(buffer, { type: "buffer" });
     const sheet = workbook.Sheets[workbook.SheetNames[0]];
-    const rows = XLSX.utils.sheet_to_json<{ image: string; text: string }>(
-      sheet
-    );
+    const rows = XLSX.utils.sheet_to_json<{
+      Image: string;
+      Text: string;
+      TextFontSize: string;
+      FontWeight: string;
+      Price?: number;
+      Brand?: string;
+      Color?: string;
+      PriceColor?: string;
+      PriceFontSize?: string;
+      BrandColor?: string;
+      BrandFontSize?: string;
+      FontFamily?: string;
+    }>(sheet);
 
     if (!rows.length) {
       return NextResponse.json(
@@ -45,12 +57,12 @@ export async function POST(req: NextRequest) {
     await Promise.all(
       rows.map(async (row, index) => {
         try {
-          if (!row.image || !row.text) {
+          if (!row.Image || !row.Text) {
             console.warn(`Row ${index + 1} missing image or text. Skipping...`);
             return;
           }
 
-          const response = await fetch(row.image, {
+          const response = await fetch(row.Image, {
             headers: {
               "User-Agent": "Mozilla/5.0",
             },
@@ -58,27 +70,101 @@ export async function POST(req: NextRequest) {
 
           if (!response.ok) {
             console.warn(
-              `Failed to fetch image URL at row ${index + 1}: ${row.image}`
+              `Failed to fetch image URL at row ${index + 1}: ${row.Image}`
             );
             return;
           }
 
           const imageBuffer = await response.buffer();
 
+          //calculating image size
           const metadata = await sharp(imageBuffer).metadata();
           const width = metadata.width ?? 500;
           const height = metadata.height ?? 500;
 
+          //defining font familyh
+          const fontFamily = row?.FontFamily || "WinkyRough-Black";
+          const fontPath = join(process.cwd(), "fonts", `${fontFamily}.ttf`);
+          const fontData = readFileSync(fontPath).toString("base64");
+          console.log("fontpath: ",fontFamily.split('-')?.[0]);
+
+          //indian currency formatting
+          const formattedPrice = new Intl.NumberFormat("en-IN").format(
+            row?.Price as number
+          );
+          const priceWithCurrency = `₹${formattedPrice}`;
+
+          //Main text
+          const lines = row.Text.split(/\r\n|\r|\n/);
+          const tspanElements = lines
+            .map(
+              (line, i) => `
+  <tspan x="50%" dy="${i === 0 ? 0 : 110}" text-anchor="middle">${line}</tspan>
+`
+            )
+            .join("");
+
+          //creating svg to add on image
           const svgText = `
   <svg width="${width}" height="${height}">
+  <defs>
+      <style type="text/css">
+        @font-face {
+          font-family: '${fontFamily}';
+           src: url('data:font/ttf;base64,${fontData}') format('truetype');
+           font-weight: ${row?.FontWeight};
+           font-display: swap;
+        }
+        .text {
+          font-family: '${fontFamily}';
+          font-size: ${row?.TextFontSize};
+          font-weight:  ${row?.FontWeight};
+        }
+             .price {
+          font-family: '${fontFamily}';
+          font-size: ${row?.PriceFontSize};
+          font-weight: ${row?.FontWeight};
+        }
+             .brand {
+          font-family: '${fontFamily}';
+          font-size: ${row?.BrandFontSize};
+          font-weight:  ${row?.FontWeight};
+        }
+      </style>
+    </defs>
     <rect width="100%" height="100%" fill="transparent"/>
-    <text x="50%" y="${
-      height - 80
-    }" font-size="60" fill="white" text-anchor="middle" dominant-baseline="middle">${
-            row.text
-          }</text>
+     <text 
+      x="50%" 
+      y="887" 
+      text-anchor="middle" 
+      dominant-baseline="middle"
+       class="text"
+         fill="${row?.Color}"
+       >
+     ${tspanElements}
+    </text>
+     <text 
+      x="50%" 
+      y="100" 
+      text-anchor="middle" 
+      dominant-baseline="middle"
+       class="price"
+         fill="${row?.PriceColor || row?.Color}"
+       >
+     ${priceWithCurrency}
+    </text>
+     <text 
+      x="50%" 
+      y="500" 
+      text-anchor="middle" 
+      dominant-baseline="middle"
+       class="brand"
+       fill="${row?.BrandColor || row?.Color}"
+       >
+     ${row.Brand}
+    </text>
   </svg>`;
-
+  
 
           const finalImage = await sharp(imageBuffer)
             .composite([{ input: Buffer.from(svgText), top: 0, left: 0 }])
